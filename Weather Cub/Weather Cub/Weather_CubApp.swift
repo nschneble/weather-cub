57be936c01b06e729806e66af2e6c4d8e161ee8f
Nick Schneble <nschneble@users.noreply.github.com>
2025-08-13 15:44:46 -0400

    create macOS app in Xcode

---
+++ Weather Cub/Weather Cub/Weather_CubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_CubApp.swift
+//  Weather Cub
+//
+//  Created by Nick Schneble on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_CubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
